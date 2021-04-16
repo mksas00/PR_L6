65e852aafddbd5c8bfebd874649a57d35fd96e52
mikolaj.sas <beqs171@gmail.com>
2021-04-16 15:46:15 +0200

    lab nr 6

---
+++ src/components/questions.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Question = (props: any) => {
+
+    return (
+        <div>
+            <p>Question:
+                {props.currentIndex}/{props.allQuestions}
+            </p>
+            <p>{props.currentQuestion}</p>
+        </div>
+    )
+};
+
+export default Question;
